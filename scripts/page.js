d24b43c0e9301eb49f9974ff39ce023480107675
Anna <a.chaikalemberg@gmail.com>
2017-06-24 16:41:32 +0300

    Adds working prototype

---
+++ scripts/page.js
@@ -0,0 +1,19 @@
+var $mapSelect = $('.mapSelect');
+$mapSelect.hide();
+//
+$('#start').click(createBattleField);
+
+function createBattleField() {
+  var $battlefield = $('.battlefield');
+  $battlefield.empty(); // clears page from headings and start button
+  $('.begin')[0].play();
+$mapSelect.show();
+  var $scene = $('<canvas/>',{
+    id: 'scene'          // creates canvas
+  });
+  $scene.appendTo($battlefield); // and adds it to the page
+
+
+  map();
+}
+
